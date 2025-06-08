4f2d9aa64e34fa095f2d1d0514061966ffab49d4
ivanvargass <165545622+IvanVargas21@users.noreply.github.com>
2025-06-09 02:25:30 +0800

    Initial Commit

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+// import { div } from "motion/react-client";
+// import Image from "next/image";
+import {HeroSection} from "../components/HeroSection";
+
+export default function Home() {
+  return (
+      <div>
+        {/* <HeroSection/> */}
+        {/* <h1>Hello</h1> */}
+      </div>
+  );
+}
---
+++ frontend/src/components/HeroSection.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function HeroSection() {
+  return (
+    <div className="relative flex h-[40rem] w-full overflow-hidden rounded-md bg-black/[0.96] antialiased md:items-center md:justify-center">
+
+    </div>
+  );
+}
