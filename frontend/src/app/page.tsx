b9a4f1e183696777278ae49597758c14d1896144
ivanvargass <165545622+IvanVargas21@users.noreply.github.com>
2025-07-05 14:54:01 +0800

    Added: TechStackSection

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/layout/Navbar";
 import StickyLabel from "@/components/ui/StickyLabel";
 import HeroSection from "@/components/HeroSection";
+import TechStackSection from "@/components/sections/Tech";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
               <Navbar/>
               <div className="max-w-7xl mx-auto px-8 xs:pt-[calc(100vh-90vh)]">
                   <HeroSection/>
+                  <TechStackSection/>
               </div>
           </div>
     </div>
