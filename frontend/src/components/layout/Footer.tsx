2abc9bf7adf3fa8fd15d318eb64244c469076549
ivanvargass <165545622+IvanVargas21@users.noreply.github.com>
2025-07-06 16:59:18 +0800

    Added: Footer

---
+++ frontend/src/components/layout/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="py-8 border-t border-neutral-700/80 bg-black/90">
+      <div className="container mx-auto px-4">
+        <div className="text-center">
+          <p className="text-gray-400 text-sm">
+            © 2025 Ivan Christopher D. Vargas. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
