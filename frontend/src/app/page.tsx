701e004d9833104a0a42c70e61ceb43993c9db91
ivanvargass <165545622+IvanVargas21@users.noreply.github.com>
2025-07-06 16:55:42 +0800

    Added: Github and Experience Section

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,12 @@
 import Navbar from "@/components/layout/Navbar";
 import StickyLabel from "@/components/ui/StickyLabel";
 import HeroSection from "@/components/sections/Hero";
+import GitHubContributions from "@/components/sections/GitHubContributions";
+import ExperienceSection from "@/components/sections/Experience";
 import TechStackSection from "@/components/sections/Tech";
 import ProjectSection from "@/components/sections/Project";
 import ContactSection from "@/components/sections/Contact";
+import Footer from "@/components/layout/Footer";
 
 export default function Home() {
   return (
@@ -13,10 +16,13 @@ export default function Home() {
               <Navbar/>
               <div className="max-w-7xl mx-auto px-8 xs:pt-[calc(100vh-90vh)]">
                   <HeroSection/>
+                  <GitHubContributions/>
+                  <ExperienceSection/>
                   <TechStackSection/>
                   <ProjectSection/>
                   <ContactSection/>
               </div>
+              <Footer/>
           </div>
     </div>
   );
